bc8fec8d08d4570fcf4bc7ba50a04603909eccb5
Bora Vlad <bora.li.vlad@utcluj.didatec.ro>
2021-04-01 00:53:41 +0300

    finalCommit

---
+++ proiectSSC/proiectSSC_v_3.ip_user_files/sim_scripts/clk_wiz_0/xcelium/run.f
@@ -0,0 +1,14 @@
+-makelib xcelium_lib/xpm -sv \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_cdc/hdl/xpm_cdc.sv" \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_memory/hdl/xpm_memory.sv" \
+-endlib
+-makelib xcelium_lib/xpm \
+  "C:/Xilinx/Vivado/2019.2/data/ip/xpm/xpm_VCOMP.vhd" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  "../../../../proiectSSC_v_3.srcs/sources_1/ip/clk_wiz_0/clk_wiz_0_sim_netlist.vhdl" \
+-endlib
+-makelib xcelium_lib/xil_defaultlib \
+  glbl.v
+-endlib
+
